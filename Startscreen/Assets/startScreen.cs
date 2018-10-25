6237a3db6d29c3a9aced46abbbfc3c023222f77e
Casper Hoegalmen <Church16413@gmail.com>
2018-10-25 23:59:59 +0200

    Added startscreen for the game

---
+++ Startscreen/Assets/startScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class startScreen : MonoBehaviour {
+    public GameObject button;
+public void buttonEvent()
+    {
+        if (button.activeSelf)
+        {
+            button.SetActive(false);
+        }
+        else
+            button.SetActive(true);
+    }
+}
